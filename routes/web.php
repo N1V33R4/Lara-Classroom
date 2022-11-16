aaded8329a2b64f947dc144da56f60ce50b31dc7
N1V33R4 <75680359+N1V33R4@users.noreply.github.com>
2022-11-16 16:55:53 +0700

    Hide old route

--- routes/web.php
+++ routes/web.php
@@ -13,6 +13,6 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('/{uri?}', function () {
-    return view('index');
-})->where('uri', '(.*)');
+// Route::get('/{uri?}', function () {
+//     return view('index');
+// })->where('uri', '(.*)');
