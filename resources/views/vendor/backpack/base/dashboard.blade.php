8625d34d198ee0d476ca942bb902057de6b46ade
N1V33R4 <75680359+N1V33R4@users.noreply.github.com>
2022-11-18 13:51:17 +0700

    Update dashboard to add new chart

--- resources/views/vendor/backpack/base/dashboard.blade.php
+++ resources/views/vendor/backpack/base/dashboard.blade.php
@@ -17,25 +17,25 @@
                 'type' => 'card',
                 'wrapper' => ['class' => 'col-sm-6 col-md-4 col-lg-3'],
                 'class'   => 'bg-warning text-white',
-                'content' => ['header' => 'Students', 'body' => App\Models\Student::count()]
+                'content' => ['header' => 'Students', 'body' => App\Models\Student::count() . ' people']
             ],
             [ 
                 'type' => 'card',
                 'wrapper' => ['class' => 'col-sm-6 col-md-4 col-lg-3'],
                 'class'   => 'bg-success text-white',
-                'content' => ['header' => 'Lectuers', 'body' => App\Models\Lecturer::count()]
+                'content' => ['header' => 'Lectuers', 'body' => App\Models\Lecturer::count() . ' people']
             ],
             [ 
                 'type' => 'card',
                 'wrapper' => ['class' => 'col-sm-6 col-md-4 col-lg-3'],
                 'class'   => 'bg-primary text-white',
-                'content' => ['header' => 'Departments', 'body' => App\Models\Department::count()]
+                'content' => ['header' => 'Departments', 'body' => App\Models\Department::count() . ' departments']
             ],
             [ 
                 'type' => 'card',
                 'wrapper' => ['class' => 'col-sm-6 col-md-4 col-lg-3'],
                 'class'   => 'bg-danger text-white',
-                'content' => ['header' => 'Rooms', 'body' => App\Models\Room::count()]
+                'content' => ['header' => 'Rooms', 'body' => App\Models\Room::count() . ' rooms']
             ]
         ]
     ];
@@ -53,6 +53,16 @@
                 'content' => [
                     'header' => 'Department Batches',
                     'body'   => 'The number of batches each department has.<br><br>',
+                ]
+            ],
+            [
+                'type' => 'chart',
+                'controller' => \App\Http\Controllers\Admin\Charts\ClassroomChartController::class,
+                'class'   => 'card mb-2',
+                'wrapper' => ['class'=> 'col-md-6'],
+                'content' => [
+                    'header' => 'Department Classrooms',
+                    'body'   => 'The number of classrooms each department has (Current | Finished).<br><br>',
                 ]
             ],
         ]
